4fdeebc0f9a7122f05ea063148f427da9a1f999d
kiisann <fikisulistiawan@gmail.com>
2025-06-14 20:21:42 +0700

    set porto

--- main.cpp
+++ main.cpp
@@ -35,3 +35,25 @@ public:
     void urutkanBerdasarkanVolatilitas();
     void optimasiPortofolio();
 };
+
+Portofolio::Portofolio() {
+    inisialisasiData();
+}
+
+void Portofolio::inisialisasiData() {
+    Saham saham[] = {
+        {"BBCA", "Finansial", 0.012, 0.02, 410000},
+        {"BNGA", "Finansial", 0.018, 0.035, 1300},
+        {"BMRI", "Finansial", 0.010, 0.015, 7200},
+        {"GOTO", "Teknologi", 0.020, 0.05, 300},
+        {"MAPI", "Teknologi", 0.008, 0.012, 6000},
+        {"ICBP", "Konsumer", 0.011, 0.018, 8000},
+        {"UNVR", "Konsumer", 0.009, 0.012, 44000},
+        {"MYOR", "Konsumer", 0.015, 0.022, 7200},
+        {"PGAS", "Energi", 0.007, 0.025, 1200},
+        {"MEDC", "Energi", 0.014, 0.03, 1200},
+        {"JSMR", "Infrastruktur", 0.013, 0.02, 4000},
+        {"WIKA", "Infrastruktur", 0.01, 0.024, 2000}
+    };
+    for (auto& s : saham) tambahSaham(s);
+}
